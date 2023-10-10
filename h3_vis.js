d7c560d6af66c94d5641686c35674e0e4f9af31f
loisfionaw <145642327+loisfionaw@users.noreply.github.com>
2023-10-10 19:56:20 +1100

    Add files via upload

---
+++ h3_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "homework3.vg.json";
+vegaEmbed("#cases_map", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "homework4.vg.json";
+vegaEmbed("#cases_graph", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "map2.vg.json";
+vegaEmbed("#map2", vg_3).then(function(result) {
+
+}).catch(console.error);
